e4ce3624cb8670c45322f6efe2ee47612a5ae671
Nagendra Singh <nagendras.chahar@gmail.com>
2019-04-27 12:21:05 +0530

    Update sendSms.js

--- router/sendSms.js
+++ router/sendSms.js
@@ -2,7 +2,7 @@
 var Request = require("request");
 
 var getStudentCode = (msz, phone) => {
-    const smsApi = `http://mysmsshop.in//http-api.php?username=RSASPS&password=Master@123&senderid=RSASPS&route=1&unicode=2&number=${phone}&message=${msz}`;
+    const smsApi = `http://mysmsshop.in//http-api.php?username=test&password=test&senderid=RSASPS&route=1&unicode=2&number=${phone}&message=${msz}`;
 
      Request.get(smsApi, (error, response, body) => {
          if(error) {
